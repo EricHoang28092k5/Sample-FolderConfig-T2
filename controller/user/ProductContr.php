221e0ded6eb06738f6b29525ca6313f5de585ca3
Nguyễn Tấn Dũng <156091609+Wolf0170@users.noreply.github.com>
2025-10-28 11:12:56 +0700

    First Commit

--- controller/user/ProductContr.php
+++ controller/user/ProductContr.php
@@ -1,21 +1,30 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/web/class/user/ProductClass.php"; 
 
-class ProductContr extends ProductClass{
+class ProductContr extends ProductClass {
 
     public function showAllProducts(){
         $result = $this->getAllProducts();
 
         if($result && $result->num_rows > 0){
             $products = [];
-            while($row = $result->fetch_assoc())
+            while($row = $result->fetch_assoc()) {
                 $products[] = $row;
-        }else{
+            }
+        } else {
             echo "Truy vấn SQL thất bại";
             exit();
         }
 
         return $products;
+    }
+
+    public function getProductsByPage($keyword, $maLoaiSP, $minPrice, $maxPrice, $limit, $offset) {
+        return parent::getProductsByPage($keyword, $maLoaiSP, $minPrice, $maxPrice, $limit, $offset);
+    }
+
+    public function countProducts($keyword, $maLoaiSP, $minPrice, $maxPrice) {
+        return parent::countProducts($keyword, $maLoaiSP, $minPrice, $maxPrice);
     }
 }
 ?>
